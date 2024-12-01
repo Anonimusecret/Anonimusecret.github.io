b44eff59ef2ebdaea4eff4f5fae6acad195e7280
Void <secretceeper25@gmail.com>
2024-12-01 14:49:38 +0300

    Контроллер сверстан

--- main.js
+++ main.js
@@ -3,43 +3,36 @@ let hidden = document.getElementById('hidden');
 let animated = document.getElementById('animated');
 let numb = document.getElementById('numb')
 
-
 let circular = document.getElementById("circular")
 let circle = document.getElementById("circle")
 let leftProgress = document.getElementById("leftProgress")
 let rihgtProgres = document.getElementById("rightProgress")
 
-let animationStyles =  `
-    @keyframes rotation{
-        0% {
-            transform: rotate(-90deg);
-        }
-        50% {
-            transform: rotate(90deg);
-        }
-        100% {
-            transform: rotate(270deg);
-        }
-    }
-`
 
 value.addEventListener('change', ()=> {
     console.log(value.value)
-    //circle.style.transform = `rotate(${value.textContent})`;
 
     numb.textContent = value.value + "%";
     if(value.value > 100 ){
+
         leftProgress.style.transform = `rotate(0.5turn)`;
         rihgtProgres.style.transform = `rotate(0.5turn)`;
+
     }else if(value.value < 0 ){
+
         leftProgress.style.transform = `rotate(0turn)`;
         rihgtProgres.style.transform = `rotate(0turn)`;
+
     }else if(value.value > 50){
+
         leftProgress.style.transform = `rotate(0.5turn)`;
         rihgtProgres.style.transform = `rotate(${(value.value-50)/100}turn)`;
+
     }else{
+
         leftProgress.style.transform = `rotate(${value.value/100}turn)`;
         rihgtProgres.style.transform = `rotate(0turn)`;
+        
     }
 })
 
